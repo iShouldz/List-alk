fa3852f09ae64624b5fe0bbb8be6a33fdfde308f
Pedro Souza <pedrosport10@gmail.com>
2024-02-05 18:44:57 -0300

    FEAT: Ajustando logica para login no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { loginActions } from "../../store/login/loginSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const loginId = useSelector((state) => state.login.currentLogin);
+  const dispatch = useDispatch();
+
+  const handleNavigate = (url) => {
+    navigate(url);
+  };
+
+  const handleLogout = () => {
+    dispatch(loginActions.handleSetCurrentLogin(""));
+    dispatch(loginActions.handleSetCurrentUsername(""));
+  };
+
   return (
     <header>
       <img src="" alt="icon" />
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
 
-      <button>Login</button>
-      <button>Register</button>
+      {!loginId ? (
+        <>
+          <button onClick={() => handleNavigate("login")}>Login</button>
+          <button onClick={() => handleNavigate("signup")}>Register</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      )}
     </header>
   );
 };
