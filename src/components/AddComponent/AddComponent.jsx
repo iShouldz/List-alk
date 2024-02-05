0f65803544109d570dc4f0c837146d9f3a9774c8
Pedro Souza <pedrosport10@gmail.com>
2024-02-05 20:47:35 -0300

    FEAT: Componente para adicionar item ao todo

---
+++ src/components/AddComponent/AddComponent.jsx
@@ -0,0 +1,59 @@
+import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    newItem: yup.string().required(),
+  })
+  .required();
+
+const AddComponent = ({ actionButton }) => {
+  const currentLogin = useSelector((state) => state.login.currentLogin);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleSubmitAdd = (data) => {
+    const { newItem } = data;
+
+    fetch(`http://localhost:3000/users/${currentLogin}`)
+      .then((response) => response.json())
+      .then((user) => {
+        user.todo[0].todoItems.push(newItem);
+        console.log(user)
+        return fetch(`http://localhost:3000/users/${currentLogin}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ todo: user.todo }),
+        });
+      })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Erro:", error));
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit(handleSubmitAdd)}>
+        <label>
+          New item
+          <input type="text" {...register("newItem")} />
+        </label>
+
+        <button>Cancel</button>
+        <button>Save list</button>
+      </form>
+    </section>
+  );
+};
+
+export default AddComponent;
--- src/store/todo/todoSlice.js
+++ src/store/todo/todoSlice.js
@@ -1,11 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialTodoState = { todoList: [] };
+const initialTodoState = {
+    todoList: [], todoSelected: {}
+};
+// 1 - atualizar todoList pelo meu todo, sempre q eu for para dashboard
+// 2- 
 
 const todoSlice = createSlice({
     name: 'todo',
     initialState: initialTodoState,
     reducers: {
+        handleGetTodoDetails(state, action) {
+            const todoIndex = state.todoList.findIndex(
+                (plant) => plant.id === action.payload
+            );
 
+            if (todoIndex >= 0) {
+                state.todoSelected = state.todoList[todoIndex];
+            } else {
+                state.todoSelected = false;
+            }
+        },
+        handleUpdateTodoList(state, action){
+            state.todoList = action.payload
+        },
+        handleSelectedTodo(state, action){
+            state.todoSelected = action.payload
+        }
     }
 })
 
