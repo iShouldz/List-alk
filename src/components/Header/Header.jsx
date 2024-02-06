4fb5e6233af6ef99fe84da0d8ad0c8b73eec190d
Pedro Souza <pedrosport10@gmail.com>
2024-02-05 22:37:18 -0300

    FEAT: Estilos para o header e home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { loginActions } from "../../store/login/loginSlice";
-
+import styles from "./styles.module.css";
+import pencil from "../../assets/pencil.svg";
 const Header = () => {
   const navigate = useNavigate();
   const loginId = useSelector((state) => state.login.currentLogin);
@@ -17,17 +18,31 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <img src="" alt="icon" />
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/dashboard">Dashboard</Link>
+    <header className={styles.headerContainer}>
+      <img src={pencil} alt="icon" />
+
+      <div className={styles.linksContainer}>
+        <Link to="/" id={styles.links}>
+          Home
+        </Link>
+        <Link to="/about" id={styles.links}>
+          About
+        </Link>
+        {loginId && <Link to="/dashboard">Dashboard</Link>}
+      </div>
 
       {!loginId ? (
-        <>
-          <button onClick={() => handleNavigate("login")}>Login</button>
-          <button onClick={() => handleNavigate("signup")}>Register</button>
-        </>
+        <div className={styles.btnContainer}>
+          <button onClick={() => handleNavigate("login")} id={styles.btnLogin}>
+            Login
+          </button>
+          <button
+            onClick={() => handleNavigate("signup")}
+            id={styles.btnRegister}
+          >
+            Register
+          </button>
+        </div>
       ) : (
         <>
           <button onClick={handleLogout}>Logout</button>
