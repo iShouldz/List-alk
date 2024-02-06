87facbb7cb5d3066a4edfbae2dbc8a6ece946ba6
Pedro Souza <pedrosport10@gmail.com>
2024-02-06 00:04:15 -0300

    FIX: Correções de componente

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { loginActions } from "../../store/login/loginSlice";
 import styles from "./styles.module.css";
 import pencil from "../../assets/pencil.svg";
+import ButtonComponent from "../UI/ButtonComponent/ButtonComponent";
 const Header = () => {
   const navigate = useNavigate();
   const loginId = useSelector((state) => state.login.currentLogin);
@@ -28,24 +29,33 @@ const Header = () => {
         <Link to="/about" id={styles.links}>
           About
         </Link>
-        {loginId && <Link to="/dashboard">Dashboard</Link>}
+        {loginId && (
+          <Link to="/dashboard" id={styles.links}>
+            Dashboard
+          </Link>
+        )}
       </div>
 
       {!loginId ? (
         <div className={styles.btnContainer}>
-          <button onClick={() => handleNavigate("login")} id={styles.btnLogin}>
+          <ButtonComponent
+            onClick={() => handleNavigate("login")}
+            color="#FFE8D6"
+          >
             Login
-          </button>
-          <button
+          </ButtonComponent>
+          <ButtonComponent
             onClick={() => handleNavigate("signup")}
-            id={styles.btnRegister}
+            color="#FFBF68"
           >
             Register
-          </button>
+          </ButtonComponent>
         </div>
       ) : (
         <>
-          <button onClick={handleLogout}>Logout</button>
+          <ButtonComponent onClick={handleLogout} color="#FFBF68">
+            Logout
+          </ButtonComponent>
         </>
       )}
     </header>
