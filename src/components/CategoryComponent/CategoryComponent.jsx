a23ca2d960e73530c553361c30759f9d4b2f0eb9
Pedro Souza <pedrosport10@gmail.com>
2024-02-06 14:24:17 -0300

    FEAT: Criando componente para o select

---
+++ src/components/CategoryComponent/CategoryComponent.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable no-unused-vars */
+import { useSelector } from "react-redux";
+import styles from "./styles.module.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    select: yup.string().required(),
+  })
+  .required();
+
+const CategoryComponent = () => {
+  const currentLogin = useSelector((state) => state.login.currentLogin);
+  const indexArray = useSelector((state) => state.todo.todoArraySelected);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+
+  const handleSubmitSelect = (data) => {
+    console.log(data)
+    const {select} = data
+    fetch(`http://localhost:3000/users/${currentLogin}`)
+      .then((response) => response.json())
+      .then((user) => {
+        console.log(user);
+        user.todo[indexArray].category = select;
+
+        return fetch(`http://localhost:3000/users/${currentLogin}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ todo: user.todo }),
+        });
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.error("Erro:", error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleSubmitSelect)} className={styles.formSelect}>
+      <label id={styles.label}>Category:</label>
+      <select name="choice" className={styles.selectComponent} {...register("select", { required: true })}>
+        <option value="filmes">Filmes</option>
+        <option value="musicas">Músicas</option>
+        <option value="livros">Livros</option>
+        <option value="series">Séries</option>
+        <option value="viagens">Viagens</option>
+        <option value="receitas">Receitas</option>
+        <option value="exercicios">Exercícios</option>
+        <option value="hobbies">Hobbies</option>
+      </select>
+
+      <button type="submit">submit</button>
+    </form>
+  );
+};
+
+export default CategoryComponent;
