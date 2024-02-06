38fbc91de40abee1ca315138040fbcfeb4c4ad17
Pedro Souza <pedrosport10@gmail.com>
2024-02-05 23:29:32 -0300

    FEAT: Estilos para o register

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,12 +2,14 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import styles from "./styles.module.css";
+import ButtonComponent from "../../components/UI/ButtonComponent/ButtonComponent";
 
 const schema = yup
   .object({
     username: yup.string().required(),
     email: yup.string().required(),
-    password: yup.string().required()
+    password: yup.string().required(),
   })
   .required();
 
@@ -21,70 +23,85 @@ const Signup = () => {
   });
 
   const handleRegister = (data) => {
-    console.log(data)
-    const {username, email, password} = data
+    console.log(data);
+    const { username, email, password } = data;
 
     const userData = {
-        username,
-        email,
-        password,
-        todo: [
-
-        ]
-    }
+      username,
+      email,
+      password,
+      todo: [],
+    };
 
     fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-  
-        body: JSON.stringify(userData),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          if (data.id) {
-            console.log("Form submitted successfully");
-          } else {
-            throw new Error("Failed to submit form");
-          }
-        })
-        .catch((error) => {
-          console.error("Form submission error:", error);
-        });
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
+      body: JSON.stringify(userData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.id) {
+          console.log("Form submitted successfully");
+        } else {
+          throw new Error("Failed to submit form");
+        }
+      })
+      .catch((error) => {
+        console.error("Form submission error:", error);
+      });
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit(handleRegister)}>
+    <section className={styles.signupContainer}>
+      <div className={styles.textRegister}>
+        <h1>Register Now</h1>
+        <h3>to be a part of the list world.</h3>
+      </div>
+
+      <form
+        onSubmit={handleSubmit(handleRegister)}
+        className={styles.formContainer}
+      >
         <label>
-          Username
+          Username <span>*</span>
           <input
             type="text"
             name="username"
             id="username"
             {...register("username")}
+            placeholder="Enter your chosen username"
           />
         </label>
 
         <label>
-          E-mail address
-          <input type="text" name="email" id="email" {...register("email")} />
+          E-mail address <span>*</span>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            {...register("email")}
+            placeholder="Enter your best e-mail"
+          />
         </label>
 
         <label>
-          Password
+          Password <span>*</span>
           <input
             type="password"
             name="password"
             id="password"
             {...register("password")}
+            placeholder="Enter a strong password"
           />
         </label>
 
-        <button type="submit">Register</button>
+        <ButtonComponent type="submit" color="#FF9F1C">
+          Register
+        </ButtonComponent>
       </form>
     </section>
   );
